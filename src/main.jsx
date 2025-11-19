5e05dca283941f8ff20a3fd545f41d006a788a53
hasaannn327-source <hasaannn327@gmail.com>
2025-11-19 21:01:20 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// PWA Kaydını Buradan Çağırıyoruz
+import { registerSW } from 'virtual:pwa-register'
+
+// Service Worker'ı Kaydet
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("Yeni içerik mevcut. Yenilemek ister misiniz?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("Uygulama çevrimdışı kullanıma hazır!");
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
